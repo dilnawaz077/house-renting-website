34f0bea209427036fef2e08d95c4e505f18a2371
dilnawaz077 <modilnawaz2512@gmail.com>
2026-06-09 15:55:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://homequest-x15k.onrender.com",
     credentials:true
 }))
 
