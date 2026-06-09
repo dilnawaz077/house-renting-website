f466855afef57eee8e3ac80f9cef640afaba647c
dilnawaz077 <modilnawaz2512@gmail.com>
2026-06-09 15:44:51 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -15,7 +15,7 @@ export const signUp = async(req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             secure:true,
-            sameSite: "non",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(201).json(user)
@@ -40,7 +40,7 @@ export const login = async (req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             secure:true,
-            sameSite: "non",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(200).json(user)
